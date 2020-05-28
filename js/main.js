b44d593a166fa70aa8bc18331900f2f048706996
tvanrys@fanshaweonline.ca <halvas_trevor@halvas.ca>
2020-05-28 12:41:55 -0400

    adding last touches for start point

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-(function() {
-  'use strict';
+(() => {
+  console.log('JS is workin!');
 
-  console.log('JS is workin!')
-
-  //let preloadScreen = document.querySelector(".preload");
-  //
   window.addEventListener("DOMContentLoaded", function() {
     console.log("DOM parsed");
   })
