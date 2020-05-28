44ee2668155ac1162e29c6a8aa386e305bddbab6
tvanrys@fanshaweonline.ca <halvas_trevor@halvas.ca>
2020-05-28 11:45:26 -0400

    some script

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@
   'use strict';
 
   console.log('JS is workin!')
+
+  //let preloadScreen = document.querySelector(".preload");
+  //
+  window.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM parsed");
+  })
 }());
